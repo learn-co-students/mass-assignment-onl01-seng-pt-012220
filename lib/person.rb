b2d83e9eba2fa51bc7ebb6a5663e31f9df114a8c
Xavier Perez <xperez007@gmail.com>
2020-02-26 15:58:43 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,39 @@
-class Person
-  #your code here
+class Jose
+  Jose_attributes = {name:
+  "Jose", hair_color: "Brown"}
+  
+  Jose = Person.new(Jose_attributes)
+  Jose.name => "Jose"
+  Jose.hair_color => "Brown"
+  Jose.eye_color => "Brown"
+  Jose.height => "Medium"
+  Jose.weight => "Heavy"
+  Jose.handed => "Right"
+  Jose.complexion => "Light"
+  Jose.t-shirt_size => "Medium"
+  Jose.wrist_size => "Small"
+  Jose.glove_size => "Large"
+  Jose.pant_length => "32"
+  Jose.pant_width => "32"
+end
+
+class Susan
+  Susan_attributes = {name:
+  "Susan", hair_color: "Brown"}
+  
+  Susan = Person.new(Jose_attributes)
+  Susan.name => "Jose"
+  Susan.hair_color => "Brown"
+  Susan.eye_color => "Brown"
+  Susan.height => "Medium"
+  Susan.weight => "Medium"
+  Susan.handed => "Right"
+  Susan.complexion => "Light"
+  Susan.t-shirt_size => "Medium"
+  Susan.wrist_size => "Small"
+  Susan.glove_size => "Small"
+  Susan.pant_length => "14"
+  Susan.pant_width => "14"
+end
+  
 end
